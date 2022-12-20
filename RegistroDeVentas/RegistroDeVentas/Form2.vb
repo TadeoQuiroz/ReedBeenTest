413e4ed030cb0631f940b6cac5ff9d12bb341c35
tadeo <tadeo@DESKTOP-EBMICFP>
2022-12-20 09:10:39 -0600

    Duplicando el repositorio

---
+++ RegistroDeVentas/RegistroDeVentas/Form2.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+End Class
